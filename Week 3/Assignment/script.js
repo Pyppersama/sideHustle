a43cad0b1a4a6d9ad70e510f3c336ba16c7eaa76
Godfrey Offeh <103786881+Pyppersama@users.noreply.github.com>
2023-04-02 12:39:16 +0100

    Add files via upload

---
+++ Week 3/Assignment/script.js
@@ -0,0 +1,27 @@
+let form = document.getElementById('form');
+
+            let Fname = document.getElementById('fullName');
+            let email = document.getElementById('emailAddress');
+            let phone = document.getElementById('phoneNumber');
+            let password = document.getElementById('pwd');
+
+            form.addEventListener('submit', e => {
+                let allMessages = [];
+
+                if(Fname.value === '' || Fname.value === null){
+                    allMessages.push("Your Fullname is Required!");
+                    
+                }else if(email.value === '' || email.value === null){
+                    allMessages.push("Your Email is required!");
+                }
+                else if(phone.value === '' || phone.value === null){
+                    allMessages.push("Your Phone Nunmer is required!");
+                }
+                else if(password.value === '' || password.value === null){
+                    allMessages.push("Please input a Password!");
+                }else{
+                    allMessages.push("This was a Sussessful Registration, Congrats!!");
+                }
+
+                alert(allMessages);
+            });
